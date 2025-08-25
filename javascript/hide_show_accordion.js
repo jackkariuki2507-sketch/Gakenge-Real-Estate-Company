bee65dbefbf3a12165246672ed5dba21a5dfdade
Jack Kariuki <jackkariuki2507@gmail.com>
2025-08-25 17:10:44 +1000

    Add Buying Tips Accordion

---
+++ javascript/hide_show_accordion.js
@@ -0,0 +1,24 @@
+const cardAccordionContainer = document.querySelector(".tips_features_container");
+
+function hide_show_accordion(event){
+  const target = event.target;
+  if(!target.classList.contains("js-toggle-button")) return;
+  // console.log(target);
+  const targetParentSibling = target.parentElement.parentElement.nextElementSibling;
+  // console.log(targetParentSibling);
+  const isHidden = targetParentSibling.classList.contains("hide-container");
+  if(isHidden){
+    // target.style.display = "none";
+    target.classList.remove("fa-plus");
+    target.classList.add("fa-minus");
+    targetParentSibling.classList.add("unhide-container");
+    targetParentSibling.classList.remove("hide-container");
+  }else{
+    target.classList.add("fa-plus");
+    target.classList.remove("fa-minus");
+    targetParentSibling.classList.remove("unhide-container");
+    targetParentSibling.classList.add("hide-container");
+  }
+}
+
+cardAccordionContainer.addEventListener("click",hide_show_accordion);
