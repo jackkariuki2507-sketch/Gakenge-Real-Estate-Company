6995376a9bb6a158b5f906d081db0caf0e223974
Jack Kariuki <jackkariuki2507@gmail.com>
2025-09-01 06:47:36 +1000

    Add accordion toggle as a module

--- javascript/main-Script.js
+++ javascript/main-Script.js
@@ -4,6 +4,11 @@ import displayPropertiesDetails from '../javascript/properties.js';
 import displayTeamMembersDetails from '../javascript/team_Members.js';
 import displayClientsTestimonials from '../javascript/clients-Testimonials.js';
 import displayPropertyBuyingTips from '../javascript/property-Buying-Tips.js';
+import hide_show_accordion from '../javascript/hide_show_accordion.js';
+
+//Variables Declaration 
+const cardAccordionContainer = document.querySelector(".tips_features_container");
+
 
 //Event Listeners
 window.addEventListener("scroll",toggleNavbarBackground);
@@ -11,3 +16,4 @@ window.addEventListener("load",displayPropertiesDetails);
 window.addEventListener("load",displayTeamMembersDetails);
 window.addEventListener("load",displayClientsTestimonials);
 window.addEventListener("load",displayPropertyBuyingTips);
+cardAccordionContainer.addEventListener("click",hide_show_accordion);
