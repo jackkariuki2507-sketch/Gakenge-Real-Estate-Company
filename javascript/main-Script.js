5f08c9bd8560f0a885388c95b078e0e87ce13ea5
Jack Kariuki <jackkariuki2507@gmail.com>
2025-09-18 10:53:45 +1000

    Add overflow-x:auto to the team members cards- allow horizontal scrolling

--- javascript/main-Script.js
+++ javascript/main-Script.js
@@ -2,11 +2,11 @@
 import {toggleNavbarBackground} from '../javascript/toggleNavbarBg.js';
 import displayPropertiesDetails from '../javascript/properties.js';
 import displayTeamMembersDetails from '../javascript/team_Members.js';
-import displayClientsTestimonials from '../javascript/clients-Testimonials.js';
-import displayPropertyBuyingTips from '../javascript/property-Buying-Tips.js';
-import hide_show_accordion from '../javascript/hide_show_accordion.js';
 import displayAboutUsInfo from '../javascript/aboutUs.js';
-import getCurrentYear from '../javascript/current_Year.js';
+// import displayClientsTestimonials from '../javascript/clients-Testimonials.js';
+// import displayPropertyBuyingTips from '../javascript/property-Buying-Tips.js';
+// import hide_show_accordion from '../javascript/hide_show_accordion.js';
+// import getCurrentYear from '../javascript/current_Year.js';
 
 // async function test(){
 //   const tip = await getAboutUsInformation();
@@ -15,8 +15,8 @@ import getCurrentYear from '../javascript/current_Year.js';
 
 // test();
 //Variables Declaration 
-const cardAccordionContainer = document.querySelector(".tips-features-container");
-const spanCurrentYear = document.querySelector(".js-current-year");
+// const cardAccordionContainer = document.querySelector(".tips-features-container");
+// const spanCurrentYear = document.querySelector(".js-current-year");
 // spanCurrentYear.innerText = `
 //   ${getCurrentYear()}
 // `;
@@ -26,6 +26,6 @@ window.addEventListener("scroll",toggleNavbarBackground);
 window.addEventListener("load",displayPropertiesDetails);
 window.addEventListener("load",displayAboutUsInfo);
 window.addEventListener("load",displayTeamMembersDetails);
-window.addEventListener("load",displayClientsTestimonials);
-window.addEventListener("load",displayPropertyBuyingTips);
-cardAccordionContainer.addEventListener("click",hide_show_accordion);
+// window.addEventListener("load",displayClientsTestimonials);
+// window.addEventListener("load",displayPropertyBuyingTips);
+// cardAccordionContainer.addEventListener("click",hide_show_accordion);
